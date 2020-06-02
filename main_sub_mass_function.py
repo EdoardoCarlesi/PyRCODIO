c8ca96646c7f83fcb113a34eef26b100fb2481d2
Edoardo <gatto@nanowar.it>
2020-06-02 11:29:14 +0200

    Pickle r/w

--- main_sub_mass_function.py
+++ main_sub_mass_function.py
@@ -32,18 +32,18 @@ data_path = '/home/edoardo/CLUES/PyRCODIO/data/'
 cat_radius = 10.0e+3
 cat_center = [50.e+3] * 3
 
-# Where should we gather the halos around the LG
-lg_radius = 1.5e+3
-
 # Read the | Gyr / z / a | time conversion table
 time = rf.read_time(data_path)
 
 all_halo_mah = []
 
+# Output file base path, save all relevant halo MAHs here
+out_base_pkl = 'saved/lg_mahs_'
+
 # Now loop on all the simulations and gather data
 for code in code_run[0:1]:
 
-    for sub in sub_run[0:1]:
+    for sub in sub_run[0:5]:
         this_path = base_path + code + '/' + sub + '/'
         this_ahf = this_path + file_ahf
         this_mah = this_path + file_mah
@@ -53,22 +53,46 @@ for code in code_run[0:1]:
             print('Reading AHF file: ', this_ahf)
             halos, halo_df = rf.read_ahf_halo(this_ahf)
 
-            print('Looking for Local Group candidates...')
-            this_model = model_run[dict_model[code]]
+            out_file_pkl = out_base_pkl + code + '_' + sub + '.pkl'
 
-            # The local group model might eventually find more than one pair in the given volume
-            these_lgs = hu.find_lg(halo_df, this_model, cat_center, cat_radius)
+            if os.path.isfile(out_file_pkl):
+                print('Loading MAHs from .pkl file...')
+                out_f_pkl = open(out_file_pkl, 'rb')
+                mahs, lg = pkl.load(out_f_pkl)
+                out_f_pkl.close()
+                print('Done.')
+                print('LG properties: ')
+                lg.info()
+            else:
+                print('Looking for Local Group candidates...')
+                this_model = model_run[dict_model[code]]
 
-            these_lgs[0].info()
+                # The local group model might eventually find more than one pair in the given volume
+                these_lgs = hu.find_lg(halo_df, this_model, cat_center, cat_radius)
 
-            for this_lg in these_lgs:
-                lg_center = this_lg.geo_com()
+                # SELECT ONE LG
+                print('Found a LG candidate with properties: ')
+                these_lgs[0].info()
+                lg = these_lgs[0]
+
+                # Define a gather radius for halos around the LG
+                lg_radius = lg.LG1.r() + lg.LG2.r() + lg.r_halos()
+                lg_radius = 1.2 * lg_radius
+
+                # TODO there might be more LGs in the area but here I am selecting only one
+                #for this_lg in these_lgs:
+                lg_center = lg.geo_com()
                 id_list = hu.halo_ids_around_center(halos, lg_center, lg_radius)
 
-                print('Reading MAHs for ', len(id_list), ' halos around the LG center of mass... ')
+                print('Reading MAHs for ', len(id_list), ' halos at ', lg_radius, ' kpc/h around the LG center of mass... ')
                 mahs = rf.read_mah_halo(id_list, this_mah, time)
                 print('Done.')
             
+                print('Saving MAHs output to file: ', out_file_pkl)
+                out_f_pkl = open(out_file_pkl, 'wb')
+                pkl.dump([mahs, lg], out_f_pkl)
+                out_f_pkl.close()
+
                 '''
                 print(mahs[0].m_t())
                 print(mahs[0].m_max())
@@ -76,19 +100,3 @@ for code in code_run[0:1]:
                 print(mahs[0].formation_time())
                 print(mahs[0].last_major_merger())
                 '''
-
-
-
-
-#print(mahs[0].x_t())
-#print(mahs[0].head())
-#print(mahs[0]['ID'])
-#cols = str(mahs[0].columns)
-#row = str(mahs[0].iloc[[1]])
-#print(row.split())
-#print(cols.split())
-#mahs[0].columns = cols.split()
-#print(mahs[0].head()) 
-#print(mahs[0]['#ID(1)']) 
-#print(mahs[0]['Mvir(4)']) 
-#print(mahs[0]['HostHalo(2)']) 
