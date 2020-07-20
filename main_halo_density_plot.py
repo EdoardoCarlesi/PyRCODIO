914afb89e36bc5496637f5b8edce876c4f67d0e7
Edoardo Carlesi <gatto@nanowar.it>
2020-07-20 20:59:38 +0200

    random pics settings

--- main_halo_density_plot.py
+++ main_halo_density_plot.py
@@ -13,6 +13,7 @@ import plot_utils as pu
 import config as cfg
 import pickle as pkl
 import pandas as pd
+import numpy as np
 import tools as t
 import os
 
@@ -33,17 +34,17 @@ n_files = 1
 frac = 1.00
 units = 'kpc'
 part_types = [4]
-grid_size = 800
+grid_size = 500
 rand_seed = 1
-fig_size = 4
-version = 'fig4size800side3.0thick1.5'
+fig_size = 8
+version = ''
 show_plot = False
 velocity = False
 augment = False
 shift = False
 legend = False
+randomize = True
 vel_components = ['Vx', 'Vy', 'Vz']
-
 n_min_part = 1000
 
 # Configure the LG model and subpaths
@@ -90,18 +91,49 @@ for code in code_run:
                     else:
                         center = this_com
                         this_fout = 'output/cluster_' + version + code + '_' + sub + '.' + str(i) + '.'
+                        this_fout_rand = 'output/cluster_' + version + code + '_' + sub + '.' + str(i) + '.rand.'
 
                     print('Plot axes, z=', z_axis, ' ax0=', ax0, ' ax1=', ax1, ' center of mass: ', this_com)
             
                     try:
                         # Select a slab around a given axis, this function returns a dataframe
                         slab_part_df = t.find_slab(part_df=part_df, side=side_size, thick=thickness, center=center, reduction_factor=frac, z_axis=z_axis, rand_seed=rand_seed)
+
+                       # Do a plot only if there are enough particles
+                        if len(slab_part_df) > n_min_part:
+                            # Feed the previously chosen dataframe and plot its 2D density projection
+                            pu.plot_density(data=slab_part_df, axes_plot=[ax0, ax1], file_name=this_fout, show_plot=show_plot, legend=legend,
+                                grid_size=grid_size, margin=0.1, data_augment=augment, fig_size=fig_size, velocity=velocity, vel=vel_components)
                     except:
                         print('Could not generate a plot for: ', this_snap, '. Data read error.')
 
-                    # Do a plot only if there are enough particles
-                    if len(slab_part_df) > n_min_part:
+                    
+                   # Add some scatter to the plot properties 
+                    if randomize == True:
+                        print('Randomizing ...')
 
-                        # Feed the previously chosen dataframe and plot its 2D density projection
-                        pu.plot_density(data=slab_part_df, axes_plot=[ax0, ax1], file_name=this_fout, show_plot=show_plot, legend=legend,
-                            grid_size=grid_size, margin=0.1, data_augment=augment, fig_size=fig_size, velocity=velocity, vel=vel_components)
+                        # The randomization of the center needs to be very small - a small % on tens of kpc might shift the LG out of the frame
+                        for i, c in enumerate(center):
+                            center[i] = c - thickness * 0.25 + 0.5 * np.random.uniform()
+    
+                        this_fout = this_fout_rand 
+                        thickness0 = thickness * (0.9 + 0.4 * np.random.uniform()) 
+                        side_size0 = side_size * (0.9 + 0.4 * np.random.uniform()) 
+                        grid_size0 = int(grid_size * (0.75 + 0.5 * np.random.uniform()))
+                        fig_size0 = fig_size * (0.8 + 0.5 * np.random.uniform())
+                        frac0 = frac 
+
+                        try:
+                            print('Adding randomized plots around: ', center, ' with size: ', side_size0, ' and frac: ', frac0, ' grid: ', grid_size0, ' fig_size: ', fig_size0)
+                            slab_part_df = t.find_slab(part_df=part_df, side=side_size0, thick=thickness0, center=center, 
+                                    reduction_factor=frac0, z_axis=z_axis, rand_seed=rand_seed)
+
+                            if len(slab_part_df > n_min_part):
+                                # Feed the previously chosen dataframe and plot its 2D density projection
+                                pu.plot_density(data=slab_part_df, axes_plot=[ax0, ax1], file_name=this_fout, show_plot=show_plot, legend=legend,
+                                    grid_size=grid_size, margin=0.1, data_augment=augment, fig_size=fig_size0, velocity=velocity, vel=vel_components)
+
+                        except:
+                            print('Could not print slices for file: ', this_fout)
+
+
