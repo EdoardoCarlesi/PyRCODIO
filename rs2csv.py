15bc38dc25c37c214fd76bffe5bcf52e4f003685
Edoardo <gatto@nanowar.it>
2020-08-07 18:04:51 +0200

    Dask Dataframe operations

--- rs2csv.py
+++ rs2csv.py
@@ -19,8 +19,10 @@ import dask.dataframe as dd
 
 catalog = '/home/edoardo/CLUES/DATA/RS/out_3.list'
 catalog_compress = '/home/edoardo/CLUES/DATA/RS/out_3_csv'
-rs_df = rf.read_rs_halo(with_dask=True, read_file=catalog)
 
+print('Reading: ', catalog, ' and compressing to csv file: ', catalog_compress)
+rs_df = rf.read_rs_halo(with_dask=True, read_file=catalog)
+print('Total number of halos read: ', rs_df.shape[0].compute())
 print(rs_df.head())
 
 cols_drop = ['DescID', 'Mvir_all', 'M200b', 'M200c', 'M500c', 'M2500c', 'Xoff', 'Voff', 
@@ -30,14 +32,13 @@ rs_df = rs_df.drop(labels=cols_drop, axis=1) #, inplace=True)
 
 print('Col cut: ')
 print(rs_df.head())
-print(len(rs_df))
 
 m_cut = 4.0e+11
 
 rs_df = rs_df[rs_df['Mvir'] > m_cut]
 
+print('Total halos: ', rs_df.shape[0].compute())
 print('Mass cut: ')
-print(len(rs_df))
 print(rs_df.head())
 
 rs_df.to_csv(catalog_compress)
