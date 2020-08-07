d68748924a8eb46593275a8b1eaea9a39260fde7
Edoardo <gatto@nanowar.it>
2020-08-07 18:05:17 +0200

    local path changes

--- rs2csv.py
+++ rs2csv.py
@@ -17,8 +17,8 @@ import tools as t
 import os
 import dask.dataframe as dd
 
-catalog = '/home/edoardo/CLUES/DATA/RS/out_3.list'
-catalog_compress = '/home/edoardo/CLUES/DATA/RS/out_3_csv'
+catalog = '/srv/cosmdata/multidark/BigMD_3840_Planck1/ROCKSTAR/catalogs/out_79.list'
+catalog_compress = '/z/carlesi/STORE/MultiDark/RockStarCSV/BigMD_3840_Planck1/out_79_csv'
 rs_df = rf.read_rs_halo(with_dask=True, read_file=catalog)
 
 print(rs_df.head())
